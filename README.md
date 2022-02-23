a476b38634c626a6a58b3122ddf4ccfe52930867
jdqw210 <79405840+jdqw210@users.noreply.github.com>
2022-02-23 15:53:55 -0500

    Update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# tbuts
- Shell script utilizing twint to automate searching across multiple usernames
+# tbuts - twint batch user tweetgrabber & search
+
+A shell script utilizing twint to automate searching across multiple usernames
+ 
+This script reads from a file containing a list of usernames to loop through. It uses twint (open source twitter intelligence tool), so make sure you have that in working order before you begin. 
+
+Input text file should have usernames without '@' symbols, and 1 username per line.
--- tbuts.sh
+++ tbuts.sh
@@ -1,34 +1,36 @@
-#!/bin/sh
+#--!/bin/sh
 
-# tell user that they have started the twint batch user tweetgrabber and search script
-echo "TBUTS INIT..."
-echo""
+#--if user calls script incorrectly, let them know how to call it:
+if [ $# -ne 2 ];
+then
+	echo "Usage: $0 usernames.txt searchterm";
+	exit -1
+fi
 
-# ask user to confirm start of script, define variable $RESP
-read -p "twint batch user tweetgrabber search, shall we begin? (y/n) " RESP
+#--ask user to confirm start of script, define variable $resp:
 echo ""
-echo "This script reads from a file containing a list of usernames"
-echo "to loop through. It uses twint (open source twitter intelligence"
-echo "tool), so make sure you have that in working order before"
-echo "you begin. GLHF"
+read -p "twint batch user tweetgrabber & search, shall we begin? (y/n) " resp
+echo ""
 
-# if user enters 'y', begin loops to read/search tweets and save them
-if [ "$RESP" = "y" ]; then
+#--if user enters 'y', begin loops to read/search tweets and save them:
+if [ "$resp" = "y" ]; then
 
-	read -p "input name of .txt file to read from..." FILENAME
-	read -p "input search term to use across accounts..." SEARCH
-
-	# define variable $LINES as each line in a file $FILENAME, and start do loop
+	echo "Searching for $2 from the list $1..."
 	
-	LINES=$(cat $FILENAME)
+	#--create dir for search results text files:
+	mkdir ~/tbuts/results
+	
+	#--define variable $lines as each line in a file $1, and start do loop:
+	
+	lines=$(cat $1)
 
-	for LINE in $LINES;
+	for line in $lines;
 	
 		do
 		
-			echo "searching username $LINE, searchterm is $SEARCH..."
-			twint -s $SEARCH -u $LINE >> ~/$LINE-$SEARCH.txt
-			echo "complete, output is ~/$LINE-$SEARCH.txt"
+			echo "searching username $line, searchterm is $2..."
+			twint -s $2 -u $line >> ~/tbuts/results/$line-$2.txt
+			echo "Output is ~/tbuts/results/$line-$2.txt"
 			echo ""
 					
 		#--loop complete:
